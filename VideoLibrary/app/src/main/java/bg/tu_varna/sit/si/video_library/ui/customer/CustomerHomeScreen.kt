a20dc6deeeebad57315b6a6edfc82bcda7444bd1
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-15 00:11:51 +0200

    Updated CustomerHomeScreen.kt
    Added HomeScreenBody() composable and HomeScreenPreview() preview composable

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/customer/CustomerHomeScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/customer/CustomerHomeScreen.kt
@@ -1,9 +1,11 @@
 package bg.tu_varna.sit.si.video_library.ui.customer
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,8 +16,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import bg.tu_varna.sit.si.video_library.data.entities.Customer
@@ -36,6 +40,34 @@ fun fakeData(): List<Customer> {
             address = "Varna, Plovdiv str."
         )
     )
+}
+
+@Composable
+fun HomeScreenBody(
+    customersList: List<Customer>,
+    modifier: Modifier = Modifier
+) {
+    if (customersList.isEmpty()) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "Sorry!\n\nThere are no registered customers yet.",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            CustomersList(
+                customersList = customersList,
+                modifier = Modifier
+            )
+        }
+    }
 }
 
 @Composable
@@ -103,6 +135,14 @@ fun Customer(
                 Text(text = "Rented Movies")
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenBodyPreview() {
+    VideoLibraryTheme {
+        HomeScreenBody(fakeData())
     }
 }
 
