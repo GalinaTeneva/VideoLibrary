004dfb88966a012e9251409da3457bd11a29ad7e
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-14 12:34:36 +0200

    Add VideoLibraryDatabase
    - implemented VideoLibraryDatabase abstract class with Room
    - add DAO access methods MovieDao and CustomerDao
    - implemented a singleton pattern for persistent database

---
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/data/VideoLibraryDatabase.kt
@@ -0,0 +1,37 @@
+package bg.tu_varna.sit.si.video_library.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import bg.tu_varna.sit.si.video_library.data.dao.CustomerDao
+import bg.tu_varna.sit.si.video_library.data.dao.MovieDao
+import bg.tu_varna.sit.si.video_library.data.entities.Customer
+import bg.tu_varna.sit.si.video_library.data.entities.Movie
+import bg.tu_varna.sit.si.video_library.data.entities.RentedMovie
+
+@Database(
+    entities = [Movie::class, Customer::class, RentedMovie::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class VideoLibraryDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+    abstract fun customerDao(): CustomerDao
+
+    companion object {
+        @Volatile
+        private var Instance: VideoLibraryDatabase? = null
+
+        fun getDatabase(context: Context): VideoLibraryDatabase {
+
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    VideoLibraryDatabase::class.java,
+                    "video_library_db"
+                ).build().also { Instance = it}
+            }
+        }
+    }
+}
