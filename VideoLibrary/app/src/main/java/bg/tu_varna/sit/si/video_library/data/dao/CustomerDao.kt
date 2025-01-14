1712ef64569f4a8b1c0495ff586eb56058a9e343
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-14 11:45:37 +0200

    Add DAOs MovieDao and CustomerDao
    Add DAOs MovieDao and CustomerDao interfaces for interacting with the database. The DAOs include methods for updating, inserting, and deleting entities. There are also method for getting  an entity by id attribute and method for getting all entities from a table

---
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/data/dao/CustomerDao.kt
@@ -0,0 +1,40 @@
+package bg.tu_varna.sit.si.video_library.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import bg.tu_varna.sit.si.video_library.data.entities.Customer
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CustomerDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(customer: Customer)
+
+    @Update
+    suspend fun update(customer: Customer)
+
+    @Delete
+    suspend fun delete(customer: Customer)
+
+    @Query(
+        """
+            SELECT *
+            FROM customers
+            WHERE customerId = :id
+        """
+    )
+    suspend fun getCustomer(id: Int): Flow<Customer>
+
+    @Query(
+        """
+            SELECT *
+            FROM customers
+            ORDER BY name ASC
+        """
+    )
+    suspend fun getAllCustomers(): Flow<List<Customer>>
+}
---
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/data/dao/MovieDao.kt
@@ -0,0 +1,40 @@
+package bg.tu_varna.sit.si.video_library.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import bg.tu_varna.sit.si.video_library.data.entities.Movie
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(movie: Movie)
+
+    @Update()
+    suspend fun update(movie: Movie)
+
+    @Delete()
+    suspend fun delete(movie: Movie)
+
+    @Query(
+        """
+            SELECT *
+            FROM movies
+            WHERE movieId = :id
+        """
+    )
+    suspend fun getMovie(id: Int): Flow<Movie>
+
+    @Query(
+        """
+            SELECT *
+            FROM movies
+            ORDER BY title ASC
+        """
+    )
+    suspend fun getAllMovies(): Flow<List<Movie>>
+}
