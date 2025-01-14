936a477128cf8c4e203dbb19bd4abbc29be43401
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-14 19:02:14 +0200

    Update HomeScreen.kt
    - Added composable function HomeScreenBody() which calls the MovieList() function when the list parameter is not empty. If there are not movies HomeScreenBody() displays informative message to the user
    - Added HomeScreenPreview() composable preview function

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/home/HomeScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package bg.tu_varna.sit.si.video_library.ui.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,32 +14,63 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import bg.tu_varna.sit.si.video_library.data.entities.Movie
 import bg.tu_varna.sit.si.video_library.ui.theme.VideoLibraryTheme
 
 fun fakeMovieList(): List<Movie> {
     return listOf(
-        Movie(
-            movieId = 1,
-            title = "Harry Potter",
-            genre = "Fantasy",
-            director = "Chris Columbus",
-            price = 3.2,
-            quantity = 3
-        ),
-        Movie(
-            movieId = 2,
-            title = "Back to the Future",
-            genre = "sci-fi",
-            director = "Robert Zemeckis",
-            price = 2.50,
-            quantity = 4
-        )
+//        Movie(
+//            movieId = 1,
+//            title = "Harry Potter",
+//            genre = "Fantasy",
+//            director = "Chris Columbus",
+//            price = 3.2,
+//            quantity = 3
+//        ),
+//        Movie(
+//            movieId = 2,
+//            title = "Back to the Future",
+//            genre = "sci-fi",
+//            director = "Robert Zemeckis",
+//            price = 2.50,
+//            quantity = 4
+//        )
     )
+}
+
+@Composable
+fun HomeScreenBody(
+    movieList: List<Movie>,
+    modifier: Modifier = Modifier
+) {
+    if (movieList.isEmpty()) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "Sorry!\n\nThere are no available movies.",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize()
+        ) {
+            MoviesList(
+                movieList = movieList,
+                modifier = Modifier
+            )
+        }
+    }
 }
 
 @Composable
@@ -95,18 +128,26 @@ fun MovieItem(
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun MovieItemPreview() {
-//    VideoLibraryTheme {
-//        MovieItem(fakeMovieList()[0])
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun MovieItemPreview() {
+    VideoLibraryTheme {
+        MovieItem(fakeMovieList()[0])
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun MovieListPreview() {
     VideoLibraryTheme {
         MoviesList(fakeMovieList())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenBodyPreview() {
+    VideoLibraryTheme {
+        HomeScreenBody(fakeMovieList())
     }
 }
