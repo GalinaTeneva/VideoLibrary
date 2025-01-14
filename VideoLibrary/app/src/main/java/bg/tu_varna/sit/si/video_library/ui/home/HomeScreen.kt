7705a606b311f16cc6549baaaecccc45083fedc7
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-14 17:20:34 +0200

    Create HomeScreen
    Add CreateScreen Kotlin file in package bg.tu_varna.sit.si.video_library.ui.home.
    The file contains:
    - composable function MovieItem() which displays a movie passed as parameter
    - preview composable function MovieItemPreview() for displaying the UI design
    - helper function, which provides list of movies for tests

---
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/home/HomeScreen.kt
@@ -0,0 +1,87 @@
+package bg.tu_varna.sit.si.video_library.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import bg.tu_varna.sit.si.video_library.data.entities.Movie
+import bg.tu_varna.sit.si.video_library.ui.theme.VideoLibraryTheme
+
+fun fakeMovieList(): List<Movie> {
+    return listOf(
+        Movie(
+            movieId = 1,
+            title = "Harry Potter",
+            genre = "Fantasy",
+            director = "Chris Columbus",
+            price = 3.2,
+            quantity = 3
+        ),
+        Movie(
+            movieId = 2,
+            title = "Back to the Future",
+            genre = "sci-fi",
+            director = "Robert Zemeckis",
+            price = 2.50,
+            quantity = 4
+        )
+    )
+}
+
+@Composable
+fun MovieItem(
+    movie: Movie,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.SemiBold
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Genre: ${movie.genre}")
+                Text(text = "Director: ${movie.director}")
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Available: ${movie.quantity}")
+                Text(text = "Rental price: ${movie.price} lv")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MovieItemPreview() {
+    VideoLibraryTheme {
+        MovieItem(fakeMovieList()[0])
+    }
+}
