42ed0eb43d8b44395ff16cde91fc1eb5b8bf83b3
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-15 19:08:42 +0200

    Update RentedMoviesHomeScreen.kt
    Move GenericHomeScreenBody() composable into a column composable and add an "Add Entry" button at the top

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMoviesHomeScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMoviesHomeScreen.kt
@@ -3,10 +3,12 @@ package bg.tu_varna.sit.si.video_library.ui.rented_movies
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -60,14 +62,28 @@ fun RentedMoviesHomeScreen(
         }
     ) {
             innerPadding ->
-        GenericHomeScreenBody(
-            itemList = fakeData(),
-            emptyMessage = "Sorry!\n\nThere are no rented movies yet.",
-            modifier = Modifier.padding(innerPadding),
-            itemListContent = { rentedMoviesList ->
-                RentedMoviesList(rentedMoviesList = rentedMoviesList)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = "New Entry")
             }
-        )
+            GenericHomeScreenBody(
+                itemList = fakeData(),
+                emptyMessage = "Sorry!\n\nThere are no rented movies yet.",
+                modifier = Modifier,
+                itemListContent = { rentedMoviesList ->
+                    RentedMoviesList(rentedMoviesList = rentedMoviesList)
+                }
+            )
+        }
     }
 }
 
@@ -169,18 +185,28 @@ fun RentedMovieRow(
     }
 }
 
-@Preview
-@Composable
-fun RentedMovieRowPreview() {
-    VideoLibraryTheme {
-        RentedMovieRow(fakeData()[0])
-    }
-}
+//@Preview
+//@Composable
+//fun RentedMovieRowPreview() {
+//    VideoLibraryTheme {
+//        RentedMovieRow(fakeData()[0])
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun RentedMoviesListPreview() {
+//    VideoLibraryTheme {
+//        RentedMoviesList(fakeData())
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
-fun RentedMoviesListPreview() {
+fun RentedMoviesHomeScreenPreview(){
     VideoLibraryTheme {
-        RentedMoviesList(fakeData())
+        RentedMoviesHomeScreen(
+            onBackClick = {}
+        )
     }
 }
