352bfb8cf13f75e9a27d0537c90593ab7c8f2415
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-15 18:50:59 +0200

    Update RentedMovieInsertScreen.kt
    Add composable RentedMovieInsertScreen() with Scafffold composable that displays VideoLibraryTopAppBarr() composable

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieInsertScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieInsertScreen.kt
@@ -3,20 +3,48 @@ package bg.tu_varna.sit.si.video_library.ui.rented_movies
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import bg.tu_varna.sit.si.video_library.ui.VideoLibraryTopAppBar
 import bg.tu_varna.sit.si.video_library.ui.theme.VideoLibraryTheme
+
+@Composable
+fun RentedMovieInsertScreen(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            VideoLibraryTopAppBar(
+                title = "Rent a movie",
+                showBackButton = true,
+                modifier = modifier,
+                onBackClick = onBackClick
+            )
+        }
+    ) {
+        innerPadding ->
+        InputForm(
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
 @Composable
 fun InputForm(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier,
+        modifier = modifier.padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
 
     ){
@@ -44,13 +72,29 @@ fun InputForm(
             label = {Text("Returned on")},
             modifier = Modifier.fillMaxWidth()
         )
+        Button(
+            onClick = { },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Save")
+        }
     }
 }
 
+//@Composable
+//@Preview(showBackground = true)
+//fun InputFormPreview() {
+//    VideoLibraryTheme {
+//        InputForm()
+//    }
+//}
+
 @Composable
 @Preview(showBackground = true)
-fun InputFormPreview() {
+fun RentedMovieInsertScreenPreview() {
     VideoLibraryTheme {
-        InputForm()
+        RentedMovieInsertScreen(
+            onBackClick = {},
+        )
     }
 }
