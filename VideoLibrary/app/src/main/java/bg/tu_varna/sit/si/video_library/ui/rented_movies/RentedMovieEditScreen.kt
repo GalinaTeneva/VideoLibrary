887391993501ec4f1d4203747fb19851f1ec79c3
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-15 23:58:06 +0200

    Add RentedMovieEditScreen.kt
    InputForm() composable from RentedMovieInsertScreen.kt is moved to newly created RentedMovieEditScreen.kt The insert screen and the edit screen are using InputForm() composable

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieEditScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieEditScreen.kt
@@ -1,36 +1,29 @@
 package bg.tu_varna.sit.si.video_library.ui.rented_movies
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import bg.tu_varna.sit.si.video_library.ui.VideoLibraryTopAppBar
 import bg.tu_varna.sit.si.video_library.ui.theme.VideoLibraryTheme
 
 @Composable
-fun RentedMovieInsertScreen(
+fun RentedMovieEditScreen(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Scaffold(
         topBar = {
             VideoLibraryTopAppBar(
-                title = "Rent a movie",
+                title = "Edit record information",
                 showBackButton = true,
                 modifier = modifier,
                 onBackClick = onBackClick
             )
         }
     ) {
-        innerPadding ->
+            innerPadding ->
         InputForm(
             modifier = Modifier.padding(innerPadding)
         )
@@ -39,10 +32,10 @@ fun RentedMovieInsertScreen(
 
 @Composable
 @Preview(showBackground = true)
-fun RentedMovieInsertScreenPreview() {
+fun RentedMovieEditScreenPreview() {
     VideoLibraryTheme {
-        RentedMovieInsertScreen(
-            onBackClick = {},
+        RentedMovieEditScreen(
+            onBackClick = {}
         )
     }
 }
---
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieInputForm.kt
@@ -0,0 +1,64 @@
+package bg.tu_varna.sit.si.video_library.ui.rented_movies
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import bg.tu_varna.sit.si.video_library.ui.theme.VideoLibraryTheme
+
+@Composable
+fun InputForm(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+
+        ){
+        OutlinedTextField(
+            value = "test",
+            onValueChange = {},
+            label = { Text("Movie ID") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = "test",
+            onValueChange = {},
+            label = { Text("Customer ID") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = "test",
+            onValueChange = {},
+            label = { Text("Rented on") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = "test",
+            onValueChange = {},
+            label = { Text("Returned on") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = { },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Save")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun InputFormPreview() {
+    VideoLibraryTheme {
+        InputForm()
+    }
+}
