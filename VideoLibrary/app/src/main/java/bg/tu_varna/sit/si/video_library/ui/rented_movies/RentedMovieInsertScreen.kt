eddc5ef365ca066ece90fdbfe380cb59f0e6d8a1
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-16 00:05:15 +0200

    Refactor
    Shange card shape in RentedMovieHomeScreenBody() composable and remove unused imports from RentedMoviesHomeScreen.kt and RentedMovieInsertScreen.kt

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieInsertScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMovieInsertScreen.kt
@@ -1,17 +1,10 @@
 package bg.tu_varna.sit.si.video_library.ui.rented_movies
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import bg.tu_varna.sit.si.video_library.ui.VideoLibraryTopAppBar
 import bg.tu_varna.sit.si.video_library.ui.theme.VideoLibraryTheme
 
