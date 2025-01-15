9f77e544e493a75a95ebc96216a6791294591a0b
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2025-01-15 23:38:56 +0200

    Update RentedMoviesHomeScreen.kt
    Add OutlinedTextField composable for searching and change the UI design of the screen

--- VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMoviesHomeScreen.kt
+++ VideoLibrary/app/src/main/java/bg/tu_varna/sit/si/video_library/ui/rented_movies/RentedMoviesHomeScreen.kt
@@ -1,6 +1,9 @@
 package bg.tu_varna.sit.si.video_library.ui.rented_movies
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,14 +11,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,19 +74,50 @@ fun RentedMoviesHomeScreen(
         }
     ) {
             innerPadding ->
-        Column(
+        RentedMovieHomeScreenBody(
+            onNewEntryClick = onNewEntryClick,
+            modifier = Modifier.padding(innerPadding)
+        )
+
+    }
+}
+
+@Composable
+fun RentedMovieHomeScreenBody(
+    onNewEntryClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Button(
+            onClick = onNewEntryClick,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
+                .fillMaxWidth()
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp)
         ) {
-            Button(
-                onClick = onNewEntryClick,
+            Text(text = "New Entry")
+        }
+        Card(
+            border = BorderStroke(width = 1.dp, color = Color.Blue),
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = { Text("Search...") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon"
+                    )
+                },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Text(text = "New Entry")
-            }
+                    .padding(16.dp),
+                singleLine = true
+            )
             GenericHomeScreenBody(
                 itemList = fakeData(),
                 emptyMessage = "Sorry!\n\nThere are no rented movies yet.",
@@ -112,7 +154,8 @@ fun RentedMovieRow(
         modifier = modifier
             .fillMaxWidth()
             .padding(4.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        border = BorderStroke(width = 1.dp, color = Color.Blue)
     ) {
         Column(
             modifier = Modifier
